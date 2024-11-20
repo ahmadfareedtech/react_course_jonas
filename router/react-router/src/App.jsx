c9e0d39693c9b0b26ac0be1447ea63bd3d0799bf
ahmad_fareed_pl <a.fareed@pioneerlogics.com>
2024-11-20 18:52:29 +0500

    more work done.

--- router/react-router/src/App.jsx
+++ router/react-router/src/App.jsx
@@ -1,8 +1,39 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+import Order from "./features/order/Order"
+import CreateOrder from "./features/order/CreateOrder"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />
+  },
+  {
+    path: "/menu",
+    element: <Menu />
+  },
+  {
+    path: "/cart",
+    element: <Cart />
+  },
+  {
+    path: "/order",
+    element: <CreateOrder />
+  },
+  {
+    path: "/order/:orderId",
+    element: <Order />
+  }
+])
+
 function App() {
   return (
-    <>
+    <RouterProvider router={router}>
       <div>Hello Vite</div>
-    </>
+    </RouterProvider>
   )
 }
 
