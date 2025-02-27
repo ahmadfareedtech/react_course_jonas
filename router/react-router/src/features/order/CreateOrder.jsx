f10a13a08798531dca5baa44ce37a13be4ba1473
ahmad_fareed_pl <a.fareed@pioneerlogics.com>
2025-02-27 18:43:37 +0500

    actions added.

--- router/react-router/src/features/order/CreateOrder.jsx
+++ router/react-router/src/features/order/CreateOrder.jsx
@@ -4,6 +4,9 @@
 //     str
 //   );
 
+import { Form, redirect } from "react-router-dom";
+import { createOrder } from "../../services/apiRestaurant";
+
 const fakeCart = [
   {
     pizzaId: 12,
@@ -37,7 +40,8 @@ const CreateOrder = () => {
     <div>
       <h2>Ready to order? Let&apos;s go!</h2>
 
-      <form>
+      {/* <Form method="POST" action="/order/new"> */}
+      <Form method="POST">
         <div>
           <label>First Name</label>
           <input type="text" name="customer" required />
@@ -69,11 +73,30 @@ const CreateOrder = () => {
         </div>
 
         <div>
+          <input type="hidden" name="cart" value={JSON.stringify(cart)} />
+        </div>
+
+        <div>
           <button>Order now</button>
         </div>
-      </form>
+      </Form>
     </div>
   );
+}
+
+export const action = async ({request}) => {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'on'
+  }
+  console.log(order);
+
+  const newOrder = await createOrder(order);
+  return redirect(`order/${newOrder.id}`);
 }
 
 export default CreateOrder;
