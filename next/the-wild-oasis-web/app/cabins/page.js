c471bc85af0765def4215993104d1bbfc61a9928
ahmadfareedtech <ahmadfareed.work@gmail.com>
2025-09-03 16:58:01 +0500

    added supabase and new project for react query

--- next/the-wild-oasis-web/app/cabins/page.js
+++ next/the-wild-oasis-web/app/cabins/page.js
@@ -1,14 +1,14 @@
-import Counter from "@/app/_components/Counter";
 import CabinCard from "@/app/_components/CabinCard";
+import { getCabins } from "../_lib/data-service";
 
 export const metadata = {
   title: "Cabins",
   description: "Explore our cozy cabins at The Wild Oasis",
 };
 
-export default function Page() {
+export default async function Page() {
   // CHANGE
-  const cabins = [];
+  const cabins = await getCabins();
 
   return (
     <div>
